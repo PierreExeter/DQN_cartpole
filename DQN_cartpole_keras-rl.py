fce1bbb027b618119513f233ccf1c9003c8ead9f
PierreExeter <pierre.aumjaud@gmail.com>
2019-10-09 10:45:30 +0100

    added keras-rl version

---
+++ DQN_cartpole_keras-rl.py
@@ -0,0 +1,37 @@
+import numpy as np
+import gym
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten
+from keras.optimizers import Adam
+from rl.agents.dqn import DQNAgent
+from rl.policy import EpsGreedyQPolicy
+from rl.memory import SequentialMemory
+
+# CREATE ENVIRONMENT
+ENV_NAME = 'CartPole-v0'
+env = gym.make(ENV_NAME)
+nb_actions = env.action_space.n
+nb_states = env.observation_space.shape
+
+# NETWORK ARCHITECTURE
+model = Sequential()
+model.add(Flatten(input_shape=(1,) + nb_states))
+model.add(Dense(16))
+model.add(Activation('relu'))
+model.add(Dense(nb_actions))
+model.add(Activation('linear'))
+print(model.summary())
+
+# DEFINE AGENT
+policy = EpsGreedyQPolicy()
+memory = SequentialMemory(limit=50000, window_length=1)
+dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=10,
+target_model_update=1e-2, policy=policy)
+dqn.compile(Adam(lr=1e-3), metrics=['mae'])
+
+# TRAIN AGENT
+dqn.fit(env, nb_steps=20000, visualize=False, verbose=2)
+dqn.save_weights('dqn_{}_weights.h5f'.format(ENV_NAME), overwrite=True)
+
+# TEST AGENT
+dqn.test(env, nb_episodes=5, visualize=True)
